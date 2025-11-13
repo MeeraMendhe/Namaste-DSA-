13ed11ace7d7cec6930b66c601ac25b774796c43
Meera Mendhe <meerasmendhe@gmail.com>
2025-11-13 09:45:02 +0530

    Basic of LinkedList

---
+++ Linked List/MiddleOfLinkedList.js
@@ -0,0 +1,14 @@
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slow=head;
+    let fast=head;
+    while(fast!=null && fast.next!=null)
+    {
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+    return slow;
+};
