39a4ae1e7a8cfb2ff9c0ccdb5c8eb09511dbf18d
Meera Mendhe <meerasmendhe@gmail.com>
2025-12-02 08:58:33 +0530

    largest odd number

---
+++ Strings/largestOddNumber.js
@@ -0,0 +1,12 @@
+var largestOddNumber = function(num) {
+ let n=num.length-1;
+
+ while(n>=0){
+    if(Number(num[n])%2==1){
+        return num.substring(0,n+1)
+    }
+    n--
+ }
+ return ""
+};
+console.log(largestOddNumber("239537672423884969653287101"))
