2613b20412fbfccf5f3d8bd60700cb71cd0a99ac
Meera Mendhe <meerasmendhe@gmail.com>
2026-04-22 10:00:30 +1000

    find sum of 2 number to given target when arr is sorted

---
+++ Two Pointer and Sliding Window/twoSumSortedArray.js
@@ -0,0 +1,14 @@
+var twoSum = function(nums, target) {
+    let i=0;
+    let j=nums.length-1
+    while(i<j){
+        let sum=nums[i]+nums[j];
+        if(sum==target){
+            return [i+1,j+1]
+        }else if(sum<target){
+            i++
+        }else{
+            j--
+        }
+    }
+};
