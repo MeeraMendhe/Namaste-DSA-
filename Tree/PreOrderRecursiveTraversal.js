12ecedfb0efdd5bad7d8746ed31be11640deedfa
Meera Mendhe <meerasmendhe@gmail.com>
2026-05-14 11:49:16 +1000

    Binary Tree Postorder Traversal using Recursion

---
+++ Tree/PreOrderRecursiveTraversal.js
@@ -0,0 +1,15 @@
+var preorderTraversal = function(root) {
+    if(!root) return []
+    let ans=[]
+    let stack=[root]
+    console.log(root)
+    while(stack.length){
+      let current=stack.pop()
+      ans.push(current?.val)
+     current.right&& stack.push(current.right)
+     current.left&& stack.push(current?.left)
+    }
+    return ans
+};
+
+console.log(preorderTraversal([1,null,2,3]))
